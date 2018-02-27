715b52b7002e306b87ee2163ad5715d19feb6ce6
Wayne Temple <waynetemple@gmail.com>
2018-02-27 15:23:08 -0600

    changed project name in travis -- set up oauth

--- server/api/index.js
+++ server/api/index.js
@@ -6,7 +6,7 @@ router.use('/users', require('./users'))
 
 router.use('/products', require('./products'))
 
-router.use('/category', require('./category'))
+// router.use('/category', require('./category'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
